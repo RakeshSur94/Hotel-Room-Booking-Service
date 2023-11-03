6ee2c18729e038d748c4faf1212b9a278c66610e
Rakesh Sur <rakeshsur72@gmail.com>
2023-11-03 20:31:02 +0530

    first-commit{Project Skeleton}

---
+++ hotel-room-booking-system/hotel-service-registry/src/test/java/com/hotel/booking/system/registry/service/HotelServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.system.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
